18b0279c394f815dc5dd6a87993c5a587a1b683d
LittleGiantBaek <horowitz@hanmail.net>
2016-09-23 20:25:31 +0900

    Helloworld project

---
+++ POYS_HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWOrld");
+		System.out.println("helloworld");
+		System.out.println("commit please!!!!!!!!!!!!!!!!!");
+	}
+}
